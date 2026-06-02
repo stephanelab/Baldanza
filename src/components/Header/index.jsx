575818342ff8449bfad7469e26a22822a3ad321c
Stéphane LABROT <stephane.labrot@gmail.com>
2026-06-02 17:38:58 +0200

    Définition du header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,57 @@
+import { NavLink } from "react-router-dom"
+import logo from "../../assets/logo.png"
+import "./styles.scss"
+
 export default function Header() {
-  return <p>Header</p>
+  return (
+    <header>
+      <nav
+        id="navbar"
+        className="navbar navbar-expand-lg fixed-top navbarScroll"
+        role="navigation"
+        aria-label="Barre de navigation"
+      >
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo} alt="Logo Baldanza" width="100" height="100" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Afficher le menu de navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/">
+                  Accueil
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/prestations">
+                  Prestations
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/a-propos">
+                  À propos
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -2,7 +2,9 @@ export default function Accueil() {
   return (
     <div>
       <h1>Bonjour</h1>
-      <p>Bienvenue sur notre site !</p>
+      <p>
+        Bienvenue sur le site de <span>Baldanza</span> !
+      </p>
     </div>
   )
 }
