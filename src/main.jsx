ab37f411d00e852069e10a04a702350a8ac92fac
Stéphane LABROT <stephane.labrot@gmail.com>
2026-06-05 15:52:23 +0200

    Carte page d'accueil + fichier JSON des soins OK

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "@fortawesome/fontawesome-free/css/all.min.css"
+import "./styles/styles.scss"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -1,21 +1,55 @@
 import Banner_Image from "../../assets/Image_bannière.webp"
 import "./styles.scss"
+import soins from "../../datas/soins"
 
 export default function Accueil() {
   return (
-    <section className="banner d-flex flex-column align-items-center gap-4 py-4">
-      <h1>
-        <span>BALDANZA</span>
-      </h1>
-      <img src={Banner_Image} alt="Bannière Baldanza" />
-      <p>Horaire : du lundi au vendredi de 9h à 18h30</p>
-      <a
-        class="btn"
-        href="https://www.planity.com/baldanza-13112-la-destrousse"
-        target="_blank"
-      >
-        <h3>Prendre rendez-vous</h3>
-      </a>
-    </section>
+    <div className="accueil">
+      <section className="banner container-fluid d-flex flex-column align-items-center gap-4 py-4">
+        <h1>
+          <span>BALDANZA</span>
+        </h1>
+        <img src={Banner_Image} alt="Bannière Baldanza" />
+        <p>Horaire : du lundi au vendredi de 9h à 18h30</p>
+        <a
+          className="btn btn-lg btn-baldanza"
+          href="https://www.planity.com/baldanza-13112-la-destrousse"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Prendre rendez-vous
+        </a>
+      </section>
+      <section className="consultation_gratuite container-fluid d-flex justify-content-evenly align-items-center my-4">
+        <h2>Consultation gratuite</h2>
+        <a
+          className="btn btn-lg btn-baldanza"
+          href="https://www.planity.com/baldanza-13112-la-destrousse"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Prendre rendez-vous
+        </a>
+      </section>
+      <section className="soins py-5">
+        <div className="row row-cols-1 row-cols-md-2 g-5 px-5">
+          {soins.map((soin) => (
+            <div className="col" key={soin.id}>
+              <div className="card h-100">
+                <img
+                  src={soin.photo}
+                  className="card-img-top"
+                  alt={soin.name}
+                />
+                <div className="card-img">
+                  <h2 className="card-title text-center">{soin.name}</h2>
+                  <p className="card-text px-3">{soin.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
