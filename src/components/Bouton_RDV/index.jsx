7bff053582e5240310b2c6011e0863039e010ae5
Stéphane LABROT <stephane.labrot@gmail.com>
2026-06-08 08:51:21 +0200

    Bouton RDV placé dans les composants

---
+++ src/components/Bouton_RDV/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.scss"
+
+export default function RDV_button() {
+  return (
+    <a
+      className="btn btn-lg btn-baldanza"
+      href="https://www.planity.com/baldanza-13112-la-destrousse"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Prendre rendez-vous
+    </a>
+  )
+}
--- src/pages/Accueil/index.jsx
+++ src/pages/Accueil/index.jsx
@@ -1,6 +1,7 @@
 import Banner_Image from "../../assets/Image_bannière.webp"
 import "./styles.scss"
 import soins from "../../datas/soins"
+import RDV_button from "../../components/Bouton_RDV"
 
 export default function Accueil() {
   return (
@@ -22,14 +23,7 @@ export default function Accueil() {
       </section>
       <section className="consultation_gratuite container-fluid d-flex justify-content-evenly align-items-center my-4">
         <h2>Consultation gratuite</h2>
-        <a
-          className="btn btn-lg btn-baldanza"
-          href="https://www.planity.com/baldanza-13112-la-destrousse"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Prendre rendez-vous
-        </a>
+        <RDV_button />
       </section>
       <section className="soins py-5">
         <div className="row row-cols-1 row-cols-md-2 g-5 px-5">
