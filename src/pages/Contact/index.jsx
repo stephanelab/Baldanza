6bbf39a891323cb00080c5dca7192715c0d78940
Stéphane LABROT <stephane.labrot@gmail.com>
2026-06-10 11:24:49 +0200

    intro page contact OK

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,3 +1,33 @@
+import baldanza from "../../assets/image_banniere.webp"
+import RDV_button from "../../components/Bouton_RDV"
+import "./styles.scss"
+
 export default function Contact() {
-  return <p>Contact</p>
+  return (
+    <div className="contact">
+      <section className="renseignements py-4">
+        <h1 className="text-center">Contact</h1>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-2 align-items-center text-center">
+            <img
+              src={baldanza}
+              alt="grand logo baldanza"
+              className="col p-4"
+            ></img>
+            <div className="col py-4">
+              <p>
+                459 avenue de la Solobie
+                <br />
+                13112 La Destrousse
+                <br />
+                Tel. : 07-50-20-58-44
+              </p>
+              <p>Horaire : du lundi au vendredi de 9h à 18h30</p>
+              <RDV_button />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
